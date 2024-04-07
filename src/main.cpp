8c1d076442747552b78df0e8941ac256ebca9f7c
Victor Lin <vyl@Victors-Air.lan>
2024-04-07 15:07:20 -0400

    Add proto and link to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <httplib.h>
 #include <iostream>
 
+#include "grocery.pb.h"
+
 int main(void) {
 	httplib::Server svr;
+
 	svr.Get("/ping", [](const httplib::Request&, httplib::Response& res) {
-		res.set_content("Pong", "text/plain");
+		res.set_content("pong", "text/plain");
 	});
+	svr.Get("/ping_proto", [](const httplib::Request&, httplib::Response& res) {
+		grocery::Item item;
+		item.set_name("pong from proto");
+		res.set_content(item.name(), "text/plain");
+	});
+
 	svr.listen("localhost", 8080);
 	return 0;
 }
